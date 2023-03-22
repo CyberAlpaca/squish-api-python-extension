af6e69718cefa77d445cf60d1d957322d34b3bd5
Adakar <jakub.top@gmail.com>
2023-03-22 22:47:39 +0100

    Add new log entries logic and type
    Add log levels to the squape-report package. It allows defining LOGLEVEL
    that will impact which messages are visible in the report and wich are
    not.

--- squape-report/squape/messages.py
+++ squape-report/squape/messages.py
@@ -4,8 +4,117 @@
 # All rights reserved.
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-import test
+import sys
 from contextlib import contextmanager
+from enum import Enum
+
+import test
+
+
+class LogLevel(Enum):
+    DEBUG = 10
+    LOG = 20
+    WARNING = 30
+    FAIL = 40
+    FATAL = 50
+
+
+LOGLEVEL = LogLevel.LOG
+
+
+def __is_level_enabled(level: LogLevel) -> bool:
+    """Checks the given log level against the currently set LOGLEVEL
+
+    Args:
+        level (LogLevel): log level to check
+
+    Returns:
+        bool: True if level is higher then LOGLEVEL, False otherwise
+    """
+    return LOGLEVEL <= level
+
+
+def debug(msg: str, details: str = "") -> None:
+    """Adds a DEBUG entry to Squish's test report with the given message and detailes.
+    The message is in fact a LOG message with a DEBUG prefix. The message will be
+    visible only if the LOGLEVEL is set to DEBUG
+
+    Args:
+        msg (str): Message
+        detail (str, optional): Details of the message. Defaults to "".
+    """
+    if __is_level_enabled(LogLevel.DEBUG):
+        test.fixateResultContext(2)
+        try:
+            test.log(f"[DEBUG] {msg}", details)
+        finally:
+            test.restoreResultContext()
+
+
+def log(msg: str, details: str = "") -> None:
+    """Adds a LOG entry to Squish's test report with the given message and detailes.
+    The message will be visible only if the LOGLEVEL is set to LOG or lower
+
+    Args:
+        msg (str): Message
+        detail (str, optional): Details of the message. Defaults to "".
+    """
+    if __is_level_enabled(LogLevel.LOG):
+        test.fixateResultContext(2)
+        try:
+            test.log(msg, details)
+        finally:
+            test.restoreResultContext()
+
+
+def warning(msg: str, details: str = "") -> None:
+    """Adds a WARNING entry to Squish's test report with the given message and detailes.
+    The message will be visible only if the LOGLEVEL is set to WARNING or lower
+
+    Args:
+        msg (str): Message
+        detail (str, optional): Details of the message. Defaults to "".
+    """
+    if __is_level_enabled(LogLevel.WARNING):
+        test.fixateResultContext(2)
+        try:
+            test.warning(msg, details)
+        finally:
+            test.restoreResultContext()
+
+
+def fail(msg: str, details: str = "") -> None:
+    """Adds a FAIL entry to Squish's test report with the given message and detailes.
+    The message will be visible only if the LOGLEVEL is set to FAIL or lower
+
+    Args:
+        msg (str): Message
+        detail (str, optional): Details of the message. Defaults to "".
+    """
+    if __is_level_enabled(LogLevel.FAIL):
+        test.fixateResultContext(2)
+        try:
+            test.fail(msg, details)
+        finally:
+            test.restoreResultContext()
+
+
+def fatal(msg: str, details: str = "") -> None:
+    """Adds a FATAL entry to Squish's test report with the given message and detailes
+    then interrupts the test execution.
+    The message will be visible only if the LOGLEVEL is set to FATAL or lower
+
+    Args:
+        msg (str): Message
+        detail (str, optional): Details of the message. Defaults to "".
+    """
+    if __is_level_enabled(LogLevel.FATAL):
+        test.fixateResultContext(2)
+        try:
+            test.fatal(msg, details)
+            sys.exit(1)
+        finally:
+            test.restoreResultContext()
 
 
 @contextmanager
