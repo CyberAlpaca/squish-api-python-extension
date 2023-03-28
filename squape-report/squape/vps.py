a18e1f0a0c3ce52f5aa945469f13d4dbe9398b3a
Adakar <jakub.top@gmail.com>
2023-03-28 23:38:00 +0200

    Group test and squish modules together while reordering imports

--- squape-report/squape/vps.py
+++ squape-report/squape/vps.py
@@ -4,9 +4,8 @@
 # All rights reserved.
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-import test
-
 import squish
+import test
 
 
 def vph_property(
