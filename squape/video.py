7348fcad61896cdb16ec1618bc67e201a1b26e63
Tomasz Pawlowski <tomasz.pawlowski@gmail.com>
2023-04-26 17:18:29 +0200

    Insted of deleting videos replace it with placeholder video with a message

--- squape/video.py
+++ squape/video.py
@@ -5,7 +5,10 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 import os
+import shutil
 from contextlib import contextmanager
+from importlib import resources
+from pathlib import Path
 
 import squishinfo
 import test
@@ -55,33 +58,43 @@ def _videos_set() -> set:
 
 def _remove_videos(videos: set) -> None:
     """
-    Remove videos from the attachments directory.
+    Remove videos from the attachments directory. The original video will be replaced
+    with a placeholder video with size of 10.6 KB.
 
     Args:
         videos (set): A set of video names to remove.
+        Does nothing when the set is empty.
     """
+
+    if not len(videos):
+        return None
+
     video_dir = os.path.join(
         squishinfo.resultDir,
         squishinfo.testSuiteName,
         squishinfo.testCaseName,
         "attachments",
     )
+
+    res_container = Path(resources.files(__package__), Path("resources"))
+    empty_video = Path(res_container, "empty_video_with_message.mp4")
+
     for video_name in videos:
         test.log(f"Remove video: {video_name}")
-        file_name = os.path.join(video_dir, video_name)
-        os.remove(file_name)
+        orig_video = Path(video_dir, video_name)
+        shutil.copy(empty_video, orig_video)
 
 
 @contextmanager
 def video_capture(message: str = None, delete_on_success: bool = False) -> None:
     """Allows using Squish's video capture as context managers.
     https://doc.qt.io/squish/squish-api.html#test-startvideocapture-message
-    Optionally delete a captured video when the execution was successful (no failures)
+    Optionally remove a captured video when the execution was successful (no failures)
 
     Args:
         message (str): log a video n the test report using the specified message.
         Defaulting to None.
-        delete_on_success (bool): Whether to delete captured video
+        delete_on_success (bool): Whether to remove captured video
         when there were no failures. Defaulting to False.
 
     Example:
@@ -117,9 +130,9 @@ def video_capture(message: str = None, delete_on_success: bool = False) -> None:
                 _remove_videos(new_videos)
 
 
-def delete_videos_on_success() -> None:
+def remove_videos_on_success() -> None:
     """
-    Delete all captured videos when the execution was successful (no failures)
+    Remove all captured videos when the execution was successful (no failures)
     """
     if _failures_results_count() == 0:
         videos_to_remove = _videos_set()
