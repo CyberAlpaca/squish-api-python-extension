504c7f2177d3844707180286b64e9c8174b5c2d0
Jakub <jakub.top@gmail.com>
2023-07-05 16:31:06 +0200

    put warning into mkdoc warning block

--- squape/video.py
+++ squape/video.py
@@ -131,8 +131,9 @@ def remove_videos_on_success() -> None:
     """
     Remove all captured videos when the execution was successful (no failures)
 
-    Warning: Removing videos on success does not work with Squish 7.1.1 and below 
-    when execution is triggered outside the Squish IDE. 
+    !!! warning
+        Removing videos on success does not work with Squish 7.1.1 and below 
+        when execution is triggered outside the Squish IDE. 
     """
     if _failure_results_count() == 0:
         videos_to_remove = _videos_set()
