91842f82bb4d710ce6e128cc92c60e4145b6e59c
EduardKaverinskyi <eduard.kaverinskyi@cyberalpaca.com>
2023-08-28 12:23:41 +0200

    Improve docstrings
    - Provide more clear selector description
    - Add missing identations and examples blocks
    - Fix missing punctuation and quotation marks
    - Add examples for function (lambda and nonolambda) usage
    - Correct formating to comply with flake8

--- squape/object_tree.py
+++ squape/object_tree.py
@@ -16,22 +16,37 @@ import object
 
 
 def children(object_name: any, selector: dict = None) -> tuple:
-    """Filter the direct children of the specified object by the specified selector
+    """
+    Finds direct children of the given object.
 
     Args:
-        object_name (any): symbolic name, real name, or object reference to the parent
-        selector (dict, optional): The selector is a dictionary of properties,
-        that must match for objects to be included into the result. 
-        The values of the selector can be lambda functions as well.
-        In that case, the verification would consist of passing the candidate's key attribute into that function. 
-        The return value should always be bool.
+        object_name (any): symbolic name, real name, or object reference to the parent.
+
+        selector (dict, optional): The selector is a dictionary of key-value pairs,
+        where a key is a property of an object, and value is expected value.
+        The object will pass verification
+        if the object's property value matches the selector value.
+        Selectors also may include functions.
+        The passed functions must accept exactly 1 argument.
         Defaults to {}, which means all objects pass the verification.
 
     Returns:
-        tuple: children objects that met the selector criteria
+        tuple: children objects that met the selector criteria.
 
     Examples:
-        >>> children(object_name, {'type' : Button, 'visible' : True})
+        ```python
+        children(object_name)
+        children(object_name, {'type' : QToolButton, 'height': 50})
+        children(object_name, {'type' : 'Button', 'visible' : True})
+        children(object_name, {'type' : 'QToolButton', 'height' : lambda x: x > 25})
+
+        def height_filter_function(height: int) -> bool:
+            return height > 30 and height < 120
+        children(
+            object_name,
+            {'type' : 'QToolButton', 'height' : height_filter_function}
+        )
+        ```
     """
     if selector is None:
         selector = {}
@@ -41,17 +56,20 @@ def children(object_name: any, selector: dict = None) -> tuple:
 
 
 def find(object_name: any, selector: dict = None, max_depth: int = None) -> tuple:
-    """Recursively filter all the children of the specified object
-    by the specified selector
+    """
+    Finds all descendants of the given object.
 
     Args:
-        object_name (any): symbolic name, real name, or object reference to the parent
-        selector (dict, optional): The selector is a dictionary of properties,
-        that must match for objects to be included into the result. 
-        The values of the selector can be lambda functions as well.
-        In that case, the verification would consist of passing the candidate's key attribute into that function. 
-        The return value should always be bool.
+        object_name (any): symbolic name, real name, or object reference to the parent.
+
+        selector (dict, optional): The selector is a dictionary of key-value pairs,
+        where a key is a property of an object, and value is expected value.
+        The object will pass verification
+        if the object's property value matches the selector value.
+        Selectors also may include functions.
+        The passed functions must accept exactly 1 argument.
         Defaults to {}, which means all objects pass the verification.
+
         max_depth (int): defines maximum depth in the object structure that should be
         while looking for children.
         Defaults to None, which mean there is no depth limit.
@@ -61,10 +79,22 @@ def find(object_name: any, selector: dict = None, max_depth: int = None) -> tupl
 
     Examples:
         ```python
-        find(object)
-        find(object, {'type' : 'ToolBar'})
-        find(object, max_depth=5)
-        find(object, {'visible' : True}, max_depth=3)
+        find(object_name)
+        find(object_name, {'type' : 'ToolBar'})
+        find(object_name, max_depth=5)
+        find(object_name, {'visible' : True}, max_depth=3)
+        find(
+            object_name,
+            {'type' : 'QToolButton', 'height' : lambda x: x > 25},
+            max_depth=5
+        )
+
+        def height_filter_function(height: int) -> bool:
+            return height > 30 and height < 120
+        find(
+            object_name,
+            {'type' : 'QToolButton', 'height' : height_filter_function}
+        )
         ```
     """
     if max_depth is None:
@@ -86,15 +116,19 @@ def find(object_name: any, selector: dict = None, max_depth: int = None) -> tupl
 
 
 def find_ancestor(object_name: any, selector: dict = None):
-    """Find the first object's ancestor that matches the selector.
+    """
+    Find the first object's ancestor that matches the selector.
 
     Args:
-        object_name (any): symbolic name, real name, or object reference to the ancestor
-        selector (dict, optional): The selector is a dictionary of properties,
-        that must match for objects to be included into the result. 
-        The values of the selector can be lambda functions as well.
-        In that case, the verification would consist of passing the candidate's key attribute into that function. 
-        The return value should always be bool.
+        object_name (any): symbolic name, real name,
+        or object reference to the ancestor.
+
+        selector (dict, optional): The selector is a dictionary of key-value pairs,
+        where a key is a property of an object, and value is expected value.
+        The object will pass verification
+        if the object's property value matches the selector value.
+        Selectors also may include functions.
+        The passed functions must accept exactly 1 argument.
         Defaults to {}, which means all objects pass the verification.
 
     Returns:
@@ -102,7 +136,21 @@ def find_ancestor(object_name: any, selector: dict = None):
         None if such an ancestor does not exist.
 
     Examples:
-        >>> find_ancestor(object_name : any, {'type' : 'MyContainerType'})
+        ```python
+        find_ancestor(object_name)
+        find_ancestor(object_name, {'type' : 'MyContainerType'})
+        find_ancestor(
+            object_name,
+            {'type' : 'QToolButton', 'height' : lambda x: x > 25}
+        )
+
+        def height_filter_function(height: int) -> bool:
+            return height > 30 and height < 120
+        find_ancestor(
+            object_name,
+            {'type' : 'QToolButton', 'height' : height_filter_function}
+        )
+        ```
     """
     if selector is None:
         selector = {}
@@ -120,25 +168,35 @@ def find_ancestor(object_name: any, selector: dict = None):
 
 
 def siblings(object_name: any, selector: dict = None):
-    """Find all the siblings (direct children of the parent) objects
-    for a given object, that match the selector.
+    """
+    Find all the object's siblings.
 
     Args:
-        object_name (any): symbolic name, real name, or object reference to the parent
-        selector (dict, optional): The selector is a dictionary of properties,
-        that must match for objects to be included into the result. 
-        The values of the selector can be lambda functions as well.
-        In that case, the verification would consist of passing the candidate's key attribute into that function. 
-        The return value should always be bool.
+        object_name (any): symbolic name, real name, or object reference to the parent.
+
+        selector (dict, optional): The selector is a dictionary of key-value pairs,
+        where a key is a property of an object, and value is expected value.
+        The object will pass verification
+        if the object's property value matches the selector value.
+        Selectors also may include functions.
+        The passed functions must accept exactly 1 argument.
         Defaults to {}, which means all objects pass the verification.
 
     Returns:
         tuple: the result of search among the object tree.
 
     Examples:
-        ```
+        ```python
         siblings(object)
         siblings(object, {'enabled' : True})
+        siblings(object_name, {'type' : 'QToolButton', 'height' : lambda x: x > 25})
+
+        def height_filter_function(height: int) -> bool:
+            return height > 30 and height < 120
+        siblings(
+            object_name,
+            {'type' : 'QToolButton', 'height' : height_filter_function}
+        )
         ```
     """
     if selector is None:
@@ -155,15 +213,18 @@ def siblings(object_name: any, selector: dict = None):
 
 
 def _is_matching(object_name: any, selector: dict) -> bool:
-    """Checks if the properties of the given object match the provided selector.
+    """
+    Checks if the properties of the given object match the provided selector.
 
     Args:
-        object_name (any): symbolic name, real name, or object reference to the parent
-        selector (dict, optional): The selector is a dictionary of properties,
-        that must match for objects to be included into the result. 
-        The values of the selector can be lambda functions as well.
-        In that case, the verification would consist of passing the candidate's key attribute into that function. 
-        The return value should always be bool.
+        object_name (any): symbolic name, real name, or object reference to the parent.
+
+        selector (dict, optional): The selector is a dictionary of key-value pairs,
+        where a key is a property of an object, and value is expected value.
+        The object will pass verification
+        if the object's property value matches the selector value.
+        Selectors also may include functions.
+        The passed functions must accept exactly 1 argument.
         Defaults to {}, which means all objects pass the verification.
 
     Returns:
@@ -173,7 +234,7 @@ def _is_matching(object_name: any, selector: dict) -> bool:
         return True
 
     object_reference = _get_object_reference(object_name)
-    
+
     for key, expected_value in selector.items():
         if key == "type":
             # Type verification
@@ -184,30 +245,35 @@ def _is_matching(object_name: any, selector: dict) -> bool:
             # Object does not have given attribute
             return False
         else:
-            #object has given attribute
+            # object has given attribute
             attr = getattr(object_reference, key)
             if isinstance(expected_value, types.FunctionType):
                 # The key is a lambda function
                 lambda_function = expected_value
                 lambda_result = lambda_function(attr)
                 if not isinstance(lambda_result, bool):
-                    raise RuntimeError(f"The lambda function assossiated with a key '{key}' returned non-boolean result: {lambda_result} ({type(lambda_result)})")
+                    raise RuntimeError(
+                        f"The lambda function assossiated with a key '{key}' \
+                        returned non-boolean result: \
+                        {lambda_result} ({type(lambda_result)})"
+                    )
                 if not lambda_result:
                     return False
-                
+
             elif attr != expected_value:
                 return False
     return True
 
 
 def _get_object_reference(object_name: any) -> any:
-    """Get the object reference for the given symbolic, real names or object reference
+    """
+    Get the object reference for the given symbolic, real names or object reference.
 
     Args:
-        object_name (any): symbolic name, real name, or object reference
+        object_name (any): symbolic name, real name, or object reference.
 
     Returns:
-        object_reference: Object reference from the given object_name
+        object_reference: Object reference from the given object_name.
     """
     if isinstance(object_name, dict):
         # Symbolic name
