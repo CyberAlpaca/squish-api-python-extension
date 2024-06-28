189f216b1688083ee0cf04a022ae913fd6f1d1db
TomaszPawlowski <tomasz.pawlowski@cyberalpaca.com>
2024-06-28 12:29:31 +0200

    Update doc string squape/object_tree.py
    Co-authored-by: Jakub <jakub.topolski@cyberalpaca.com>

--- squape/object_tree.py
+++ squape/object_tree.py
@@ -26,9 +26,11 @@ def children(object_or_name: any, selector: dict) -> tuple:
         object_or_name (any): symbolic name, real name, or object reference.
 
         selector (dict): The selector is a dictionary of key-value pairs,
-            where a key is a property of an object  and value is expected value
-            or function. The passed functions must accept exactly one argument.
-            Accepted key is also 'type' and then value should by object type.
+The selector is a dictionary consisting of key-value pairs. In this dictionary:
+- Each key represents a property of an object.
+- Each value can either be an expected value for that property or a function that takes exactly one argument.
+
+Additionally, the dictionary can have a key 'type' where the value should be the expected object type.
             An object will pass verification
             if the object's property value matches the selector value.
             Defaults to {}, which means all objects pass the verification.
